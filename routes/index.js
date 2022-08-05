bbac5f662916a084629031135a477b979f14cc5e
Gonzalo <fonsofara@gmail.com>
2022-08-05 10:54:44 -0300

    cp-m3 routes exercise 07

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,22 @@ module.exports = router
 
 // Escriban sus rutas acá
 // Siéntanse libres de dividir entre archivos si lo necesitan
+router.get('/users', (req, res) => {
+    res.status(200).json(models.listUsers());
+});
+
+router.post('/users', (req, res) => {
+    const {email, name} = req.body;
+
+    let users = models.listUsers();
+    let user = users.find(u => u.email === email);
+    if(user) res.status(400).json({ error: 'El usuario ya existe'});
+    
+    models.addUser(email, name);
+    res.status(201).json({ msg: `Usuario ${email} creado correctamente`});
+});
+
 
 // Hint:  investigá las propiedades del objeto Error en JS para acceder al mensaje en el mismo.
+
+
