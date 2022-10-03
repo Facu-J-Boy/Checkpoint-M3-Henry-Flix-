bbe535c91cd2d0bcd0406f63babb996c6915d67f
Facu-J-Boy <boyfacundo45@gmail.com>
2022-10-03 20:08:34 -0300

    commit

--- models/model.js
+++ models/model.js
@@ -2,25 +2,26 @@
 /// =============================== HENRY-FLIX ================================ ///
 /// =========================================================================== ///
 
-'use strict'
+"use strict";
 
-const categories = ['regular', 'premium']
+const categories = ["regular", "premium"];
 
-let users = []
-let series = []
+//ARREGLO CON TODOS LOS USUARIOS
+let users = [];
+//ARREGLO CON TODAS LAS SERIES
+let series = [];
 
 module.exports = {
-
   reset: function () {
     // No es necesario modificar esta función. La usamos para "limpiar" los arreglos entre test y test.
 
-    users = []
-    series = []
+    users = [];
+    series = [];
   },
 
   // ==== COMPLETAR LAS SIGUIENTES FUNCIONES (vean los test de `model.js`) =====
 
-  addUser: function (email, name, plan='regular', watched=[]) {
+  addUser: function (email, name, plan = "regular", watched = []) {
     // Agrega un nuevo usuario, verificando que no exista anteriormente en base a su email.
     // En caso de existir, no se agrega y debe arrojar el Error ('El usuario ya existe') >> ver JS throw Error
     // Debe tener una propiedad <plan> que inicialmente debe ser 'regular'.
@@ -28,47 +29,59 @@ module.exports = {
     // El usuario debe guardarse como un objeto con el siguiente formato:
     // {  email: email, name: name,  plan: 'regular' , watched: []}
     // En caso exitoso debe retornar el string 'Usuario <email_del_usuario> creado correctamente'.
-    let user = users.find(u => u.email === email);
-    if(user) throw new Error('El usuario ya existe');
+    //VERIFICAMOS QUE NO EXISTA ->buscamos por el email de los usuarios
+    let user = users.find((u) => u.email === email);
+    //EN EL CASO DE QUE EXISTA, ARROJAMOS UN NUEVO ERROR
+    if (user) throw new Error("El usuario ya existe");
+    //EN EL CASO DE QUE NO EXISTA -> crear objeto del usuario
+    let newUser = {
+      email: email,
+      name: name,
+      plan: plan,
+      watched: watched,
+    };
 
-    let newUser = { 
-      email, 
-      name,  
-      plan,
-      watched
-    }
     users.push(newUser);
     return `Usuario ${newUser.email} creado correctamente`;
+    // return "Usuario " + newUser.email + " creado correctamente";
   },
 
+  //puede o NO recibir el plan por parametros
   listUsers: function (plan) {
     // Si no recibe parámetro, devuelve un arreglo con todos los usuarios.
     // En caso de recibir el parámetro <plan>, devuelve sólo los usuarios correspondientes a dicho plan ('regular' o 'premium').
-    if(plan){
-      let usersPlan = users.filter(u => u.plan === plan);
-      return usersPlan;
+
+    //plan listUsers("regular" || "premium")
+    //FILTER -> TE RETORNA EL MISMO ARREGLO PERO FILTRADO
+    if (plan) {
+      return users.filter((u) => u.plan === plan); //PLAN = STRING -> "regular" -> filtra los de plan: "regular" || plan: "premium"
     }
     return users;
   },
 
   switchPlan: function (email) {
-  // Alterna el plan del usuario: si es 'regular' lo convierte a 'premium' y viceversa.
-  // Retorna el mensaje '<Nombre_de_usuario>, ahora tienes el plan <nuevo_plan>'
-  // Ej: 'Martu, ahora tienes el plan premium'
-  // Si el usuario no existe, arroja el Error ('Usuario inexistente')
-    let user = users.find(u => u.email === email);
-    if(!user) throw new Error('Usuario inexistente');
+    // Alterna el plan del usuario: si es 'regular' lo convierte a 'premium' y viceversa.
+    // Retorna el mensaje '<Nombre_de_usuario>, ahora tienes el plan <nuevo_plan>'
+    // Ej: 'Martu, ahora tienes el plan premium'
+    // Si el usuario no existe, arroja el Error ('Usuario inexistente')
 
-    if(user.plan === 'regular'){
-      user.plan = 'premium';
+    //PRIMERO BUSCAMOS QUE EXISTA EL USUARIO a través del mail
+    let user = users.find((u) => u.email === email);
+    //EN CASO DE QUE NO EXISTA, ARROJA EL ERROR ('usuario inexistente')
+    if (!user) throw new Error("Usuario inexistente");
+
+    //SI EXISTE EL USUARIO
+    if (user.plan === "regular") {
+      //si es regular, le cambiamos el plan a Premium
+      user.plan = "premium";
       return `${user.name}, ahora tienes el plan ${user.plan}`;
-    } else {
-      user.plan = 'regular';
+    } else if (user.plan === "premium") {
+      user.plan = "regular";
       return `${user.name}, ahora tienes el plan ${user.plan}`;
     }
   },
 
-  addSerie: function (name, seasons, category, year, rating=0, reviews=[]) {
+  addSerie: function (name, seasons, category, year, rating = 0, reviews = []) {
     // Agrega una nueva serie al catálogo.
     // Si la serie ya existe, no la agrega y arroja un Error ('La serie <nombre_de_la_serie> ya existe')
     // Si la categoría no existe, arroja un Error ('La categoría <nombre_de_la_categoría> no existe') y no agrega la serie.
@@ -76,30 +89,36 @@ module.exports = {
     // Debe guardar la propiedad <category> de la serie (regular o premium)
     // Debe guardar la propiedade <rating> inicializada 0
     // Debe guardar la propiedade <reviews> que incialmente es un array vacío.
-    let serie = series.find(s=> s.name === name);
-    if(serie) throw new Error(`La serie ${serie.name} ya existe`);
-    if(!categories.includes(category)) throw new Error(`La categoría ${category} no existe`);
 
-    let newSerie = {
-      name, 
-      seasons,
-      category,
-      year,
-      rating,
-      reviews
-    }
+    const serie = series.find((s) => s.name === name);
 
+    const serieCategory = categories.includes(category);
+    
+    if(serie) throw new Error (`La serie ${name} ya existe`);
+    if(!serieCategory) throw new Error (`La categoría ${category} no existe`);
+     let newSerie = {
+      name: name,
+      seasons: seasons,
+      category: category,
+      year: year,
+      rating: 0,
+      reviews: [],
+    };
     series.push(newSerie);
-    return `La serie ${newSerie.name} fue agregada correctamente`;
+    return (`La serie ${newSerie.name} fue agregada correctamente`);
   },
 
   listSeries: function (category) {
     // Devuelve un arreglo con todas las series.
     // Si recibe una categoría como parámetro, debe filtrar sólo las series pertenecientes a la misma (regular o premium).
     // Si la categoría no existe, arroja un Error ('La categoría <nombre_de_la_categoría> no existe') y no agrega la serie.
-    if(category && !categories.includes(category)) throw new Error(`La categoría ${category} no existe`);
-    if(category && categories.includes(category)) return series.filter(s=> s.category === category);
-
+    if(category) {
+      if(category !== 'regular' && category !== 'premium') {
+        throw new Error(`La categoría ${category} no existe`)
+      } else {
+        return series.filter((s) => s.category === category)
+      }
+    }
     return series;
   },
 
@@ -111,27 +130,30 @@ module.exports = {
     // En caso de contrario arrojar el Error ('Contenido no disponible, contrata ahora HenryFlix Premium!')
     // En caso exitoso, añadir el nombre (solo el nombre) de la serie a la propiedad <watched> del usuario.
     // Devuelve un mensaje con el formato: 'Reproduciendo <nombre de serie>'
-    let user = users.find(u => u.email === email);
-    if(!user) throw new Error('Usuario inexistente');
 
-    let matchedSerie = series.find(s=> s.name === serie);
-    if(!matchedSerie) throw new Error('Serie inexistente');
+    const usuario = users.find((u) => u.email === email);
+    const show = series.find((s) => s.name === serie);
 
-    if(user.plan === 'regular' && matchedSerie.category === 'premium') {
-      throw new Error('Contenido no disponible, contrata ahora HenryFlix Premium!')
+    if(!usuario) throw new Error ('Usuario inexistente');
+    if(!show) throw new Error ('Serie inexistente');
+    if(usuario.plan === 'premium') {
+      usuario.watched.push(serie);
+      return `Reproduciendo ${serie}`;
+    }
+    if(show.category === 'regular') {
+      usuario.watched.push(serie);
+      return `Reproduciendo ${serie}`;
     } else {
-      user.watched.push(serie);
-      return `Reproduciendo ${serie}`
+      throw new Error ('Contenido no disponible, contrata ahora HenryFlix Premium!');
     }
   },
 
   watchAgain: function (email) {
     // Devuelve sólo las series ya vistas por el usuario
     // Si el usuario no existe, arroja el Error ('Usuario inexistente')
-    let user = users.find(u => u.email === email);
-    if(!user) throw new Error('Usuario inexistente');
-
-    return user.watched;
+    let usuario = users.find((u) => u.email === email);
+    if(!usuario) throw new Error ('Usuario inexistente'); 
+    return usuario.watched;
   },
 
   rateSerie: function (serie, email, score) {
@@ -143,25 +165,30 @@ module.exports = {
     // Si la serie no existe, arroja el Error ('Serie inexistente') y no actualiza el puntaje.
     // Debe recibir un puntaje entre 1 y 5 inclusive. En caso contrario arroja el Error ('Puntaje inválido') y no actualiza el puntaje.
     // Si el usuario no reprodujo la serie, arroja el Error ('Debes reproducir el contenido para poder puntuarlo') y no actualiza el puntaje. >> Hint: pueden usar la función anterior
-    let user = users.find(u => u.email === email);
-    let matchedSerie = series.find(s=> s.name === serie);
+    let usuario = users.find(u => u.email === email);
 
-    if(!user) throw new Error('Usuario inexistente');
-    if(!matchedSerie) throw new Error('Serie inexistente');
+    let show = series.find(s => s.name === serie);
+
+    if(!usuario) throw new Error('Usuario inexistente');
+
+    if(!show) throw new Error('Serie inexistente');
+
     if(score < 1 || score > 5) throw new Error('Puntaje inválido');
-    if(!user.watched.includes(serie)) throw new Error('Debes reproducir el contenido para poder puntuarlo');
 
-    let review = {
-      email,
-      score
+    if(!usuario.watched.includes(serie)) 
+      throw new Error ('Debes reproducir el contenido para poder puntuarlo');
+
+
+    let newReview = {
+      email: email,
+      score: score,
     }
-    matchedSerie.reviews.push(review);
 
-    let newRating = 0;
-    matchedSerie.reviews.forEach(r => newRating += r.score);
-    matchedSerie.rating = Math.round(newRating / matchedSerie.reviews.length);
+    show.reviews.push(newReview);
 
+    let promedio = 0;
+    show.reviews.forEach(r => promedio += r.score);
+    show.rating = (promedio / show.reviews.length);
     return `Le has dado ${score} puntos a la serie ${serie}`;
-  }
-
-}
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,114 +1,144 @@
-'use strict'
+"use strict";
 
-const models = require('../models/model')
-const express = require('express')
+/* const objeto = {
+	id: 1,
+	nombre: "gonzalo"
+}
+
+DESTRUCTURING 
+const {nombre} = objeto
+
+const query = {
+	user: "correo@correo.com",
+	name: "gonzalo"
+}
+
+const {user} = query 
+console.log(user) -> correo@correo.com
+
+const {email} = query ->UNDEFINED */
+
+const models = require("../models/model");
+
+//models.listUsers()
+const express = require("express");
 // const { response } = require('../app')
 
-const router = express.Router()
-module.exports = router
+const router = express.Router();
+module.exports = router;
 
 // Escriban sus rutas acá
 // Siéntanse libres de dividir entre archivos si lo necesitan
-router.get('/users', (req, res) => {
-    res.status(200).json(models.listUsers());
+router.get("/users", (req, res) => {
+  //NO RECIBE NADA ni por query, ni por params, ni por body
+
+  //.JSON(tenes que devolver un OBJETO || ARREGLO)
+  res.status(200).json(models.listUsers());
 });
 
-router.post('/users', (req, res) => {
-    const {email, name} = req.body;
+router.post("/users", (req, res) => {
+  const { email, name } = req.body;
 
-    let users = models.listUsers();
-    let user = users.find(u => u.email === email);
-    if(user) res.status(400).json({ error: 'El usuario ya existe'});
-    
-    models.addUser(email, name);
-    res.status(201).json({ msg: `Usuario ${email} creado correctamente`});
+  let users = models.listUsers(); //ME TRAIGO TODOS LOS USUARIOS
+  let user = users.find((user) => user.email === email); //ME FIJO SI EXISTE EL USUARIO
+
+  //si existe, retornamos error
+  if (user) res.status(400).json({ error: "El usuario ya existe" });
+
+  //SI NO EXISTE, lo agregamos
+  models.addUser(email, name);
+  res.status(201).json({ msg: `Usuario ${email} creado correctamente` });
 });
 
-router.patch('/users/plan', (req, res) => {
+router.patch("/users/plan", (req, res) => {
+  //{  email, name, apellido, direccion} -> PUT-> actualizo email, name -> {email, name} LO SOBREESCRIBE
+  //->PATCH -> actualizo email, name -> {email, name, apellido, direccion} ->SOLO SOBREESCRIBE LAS PROPIEDADES QUE ACTUALIZAMOS Y NO TODO EL OBJETO
+  const email = req.query.user; //PONGAN EL NOMBRE DE LA QUERY siempre que hagan query
+  //PARA HACER UN DESTRUCTURING -> EL NOMBRE DE LA QUERY === al nombre que le doy a la CONSTANTE -> user=email@correo.com
 
-    const email = req.query.user;
-    let users = models.listUsers();
-    let user = users.find(u => u.email === email);
+  //.patch('/users/plan?user=wan@henryflix.com') -> MI QUERY se llama user; ?[user]->nombre de la QUERY (nombre de la propiedad)
+  //=wan@henryflix.com -> lo que esta desp del = [el valor de mi query] {user: wan@henryflix.com }
+  //ME TRAIGO TODOS LOS USERS CON LA FUNCION DEL MODELO listUsers
+  let users = models.listUsers();
+  //VERIFICO SI ESE USUARIO EXISTE CON UN FIND -> email
+  let user = users.find((user) => user.email === email);
 
-    if(!user) res.status(404).json({ error: 'Usuario inexistente' });
+  //SI EL USUARIO NO EXISTE -> RETORNO EL ERROR CON EL MSJ COMO ESTA EN LOS TEST
+  if (!user) return res.status(404).json({ error: "Usuario inexistente" });
 
-    models.switchPlan(user.email);
-    res.status(200).json({ msg: `${user.name}, ahora tienes el plan ${user.plan}`});
+  //SI EL USUARIO EXISTE, LO PATCHEO (le cambio el plan)
+  models.switchPlan(user.email);
+  res
+    .status(200)
+    .json({ msg: `${user.name}, ahora tienes el plan ${user.plan}` });
 });
 
-router.get('/series', (req, res) => {
-    res.status(200).json(models.listSeries());
+router.get("/series", (req, res) => {
+  res.json(models.listSeries());
 });
 
-router.post('/series', (req, res) => {
-
-    const {name, seasons, category, year} = req.body;
-    let allSeries = models.listSeries();
-    let matchedSerie = allSeries.find(s => s.name === name);
-
-    if(matchedSerie) res.status(400).json({ error: `La serie ${name} ya existe`});
-    if(category !== 'regular' && category !== 'premium') res.status(400).json({ error: `La categoría ${category} no existe`});
-
-    models.addSerie(name, seasons, category, year);
-    res.status(201).json({ msg: `La serie ${name} fue agregada correctamente`});
+router.post("/series", (req, res) => {
+  const {name, seasons, category, year} = req.body;
+  let show = models.listSeries();
+  let serie = show.find((s) => s.name === name);
+  if(serie) res.status(400).json({error: `La serie ${name} ya existe`});
+  if(category !== 'regular' && category !== 'premium') res.status(400).json({error: `La categoría ${category} no existe`
+  });
+  models.addSerie(name, seasons, category, year)
+  res.status(201).json({msg: `La serie ${name} fue agregada correctamente`});
 });
 
-router.get('/series/:category', (req, res) => {
-    
-    const {category} = req.params;
-    if(category !== 'regular' && category !== 'premium') res.status(404).json({ error: `La categoría ${category} no existe` });
+router.get("/series/:category", (req, res) => {
+  const {category} = req.params;
 
-    res.status(200).json(models.listSeries(category));
+  if(category !== 'regular' && category !== 'premium') res.status(404).json({error: `La categoría ${category} no existe`});
+
+  res.status(200).json(models.listSeries(category));
 });
 
-router.get('/play/:serie', (req, res) => {
-
-    const {serie} = req.params;
-    const email = req.query.user;
-
-    let users = models.listUsers();
-    let user = users.find(u => u.email === email);
-    let allSeries = models.listSeries();
-    let matchedSerie = allSeries.find(s => s.name === serie);
-
-    if(!user) res.status(404).json({ error: 'Usuario inexistente' });
-    if(!matchedSerie) res.status(404).json({ error: 'Serie inexistente' });
-    if(matchedSerie.category !== user.plan) res.status(404).json({ error: 'Contenido no disponible, contrata ahora HenryFlix Premium!' });
-
-    res.status(200).json({ msg: models.play(serie, email)});
-
+router.get("/play/:serie", (req, res) => {
+  const {serie} = req.params;
+  const email = req.query.user;
+  let usuarios = models.listUsers();
+  let usuario = usuarios.find((u) => u.email === email);
+  let allSeries = models.listSeries();
+  let foundSerie = allSeries.find((s) => s.name === serie);
+  if(!usuario) res.status(404).json({error: 'Usuario inexistente'});
+  if(!foundSerie) res.status(404).json({ error: 'Serie inexistente' });
+  if(foundSerie.category === 'premium' && usuario.plan !== 'premium') res.status(404).json({ error: 'Contenido no disponible, contrata ahora HenryFlix Premium!' });
+  res.status(200).json({ msg: `Reproduciendo ${serie}` });
 });
 
-router.get('/watchAgain', (req, res) => {
-
-    const email = req.query.user;
-
-    let users = models.listUsers();
-    let user = users.find(u => u.email === email);
-    if(!user) res.status(404).json({ error: 'Usuario inexistente' });
-
-    res.status(200).json(user.watched);
+router.get("/watchAgain", (req, res) => {
+  const email = req.query.user;
+  let allUsers = models.listUsers();
+  let foundUser = allUsers.find((u) => u.email === email);
+  if(!foundUser) res.status(404).json({ error: 'Usuario inexistente' });
+  res.status(200).json(models.watchAgain(email));
 });
 
-router.post('/rating/:serie', (req, res) => {
-    const {email, score} = req.body;
-    const {serie} = req.params;
-
-    let users = models.listUsers();
-    let user = users.find(u => u.email === email);
-    let allSeries = models.listSeries();
-    let matchedSerie = allSeries.find(s => s.name === serie);
-
-    if(!user) res.status(404).json({ error: 'Usuario inexistente' });
-    if(!matchedSerie) res.status(404).json({ error: 'Serie inexistente' });
-    if(score < 1 || score > 5) res.status(404).json({ error: 'Puntaje inválido' });
-    if(!user.watched.includes(serie)) res.status(404).json({ error: 'Debes reproducir el contenido para poder puntuarlo'});
-
-    res.status(200).json({msg: models.rateSerie(serie, email, score)});
-})
-
-
+router.post("/rating/:serie", (req, res) => {
+  const {serie} = req.params;
+  const {email, score} = req.body;
+  let allUsers = models.listUsers();
+  let allSeries = models.listSeries();
+  let foundUser = allUsers.find((u) => u.email === email);
+  let foundSerie = allSeries.find((s) => s.name === serie);
+  if(!foundUser) res.status(404).json({ error: 'Usuario inexistente' });
+  if(!foundSerie) res.status(404).json({ error: 'Serie inexistente' });
+  if(score < 1 || score > 5) res.status(404).json({ error: 'Puntaje inválido' });
+  if(!foundUser.watched.includes(serie)) res.status(404).json({ error: 'Debes reproducir el contenido para poder puntuarlo' });
+  models.rateSerie(serie, email, score);
+  res.status(200).json({ msg: `Le has dado ${score} puntos a la serie ${serie}` });
+});
 
 // Hint:  investigá las propiedades del objeto Error en JS para acceder al mensaje en el mismo.
+  
 
+
+  
+  
+  
+  
+  
 
